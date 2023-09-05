95556e79842bd5dab95fbbfae837d3750e6853f0
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-09-05 18:28:51 +0200

    fix host

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@
 set -euo pipefail
 
 
-sed -i -e "s/__PASSWORD__/${DB_PASSWORD}/ /home/openolat/conf/Catalina/localhost/ROOT.xml"
-sed -i -e "s#__DB_URL__#${DB_URL}# /home/openolat/conf/Catalina/localhost/ROOT.xml"
-sed -i -e "s#__HOST__#${OLAT_HOST}# /home/openolat/conf/server.xml"
+sed -i -e "s#__PASSWORD__#${DB_PASSWORD}#" "/home/openolat/conf/Catalina/localhost/ROOT.xml"
+sed -i -e "s#__DB_URL__#${DB_URL}#" "/home/openolat/conf/Catalina/localhost/ROOT.xml"
+
+sed -i -e "s#__HOST__#${OLAT_HOST}#" "/home/openolat/conf/server.xml"
+sed -i -e "s#__HOST__#${OLAT_HOST}#" "/home/openolat/lib/olat.local.properties"
 
 exec /home/openolat/tomcat/bin/catalina.sh run
